cd678ffde4d0ce8b796c6ff7f0d0e4c18814ff9b
passinkum-coder <passinkum@gmail.com>
2026-03-27 17:38:21 -0400

    Update base_scraper.py

--- agent/scrapers/base_scraper.py
+++ agent/scrapers/base_scraper.py
@@ -1,6 +1,6 @@
 class BaseScraper:
-    def __init__(self):
-        pass
+    def __init__(self, county_name=None):
+        self.county_name = county_name
 
     def scrape(self):
         raise NotImplementedError("Scrape method must be implemented by child class")
